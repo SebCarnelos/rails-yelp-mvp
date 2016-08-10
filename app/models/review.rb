38eccafbe41c41100632064558823dcbd041089e
Sebastien Carnelos <sebastien.carnelos@gmail.com>
2016-08-10 16:37:08 +0200

    models updated + controllers created + routes launched

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, numericality: true
+  validates :rating, inclusion: (0..5).to_a
 end

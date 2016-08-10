db2c3fa48efed6ec208a0a806bdaa88ff830e485
Sebastien Carnelos <sebastien.carnelos@gmail.com>
2016-08-10 18:12:07 +0200

    still have to work on the reviews form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+.
+    redirect_to restaurant_path
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+sushi_shop = Restaurant.create({
+  name: 'Sushi Shop',
+  address: 'rue des sushis',
+  phone_number: '555-0001',
+  category: 'japanese',
+  })
+
+la_baguette_doree = Restaurant.create({
+  name: 'La Baguette Dorée',
+  address: 'rue des baguettes',
+  phone_number: '555-0002',
+  category: 'chinese',
+  })
+
+marios_pasta = Restaurant.create({
+  name: "Mario's Pasta",
+  address: 'rue des pâtes',
+  phone_number: '555-0003',
+  category: 'italian',
+  })
+
+le_bristronome = Restaurant.create({
+  name: "Le Bistronome",
+  address: 'rue des bistros',
+  phone_number: '555-0004',
+  category: 'french',
+  })
+
+moules_frites = Restaurant.create({
+  name: "Moules/Frites",
+  address: 'rue des moules',
+  phone_number: '555-0005',
+  category: 'belgian',
+  })
